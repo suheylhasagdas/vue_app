97dfe9ab7cba789c29c23a7787165ea13eb770cb
Suheyl Hasagdaş <shasagda@ford.com.tr>
2023-05-17 10:55:50 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+new Vue ({
+    el : "#app",
+    data: {
+        player_heal : 100,
+        monster_heal : 100,
+        game_is_on : false
+    },
+    methods: {
+        start_game : function(){
+            this.game_is_on = true;
+        },
+        attack : function(){
+            var point = Math.ceil(Math.random() * 10);
+            this.monster_heal -= point;
+            this.monster_attack();
+        },
+        special_attack : function(){
+            var point = Math.ceil(Math.random() * 25);
+            this.monster_heal -= point;
+            this.monster_attack();
+        },
+        heal_up : function(){
+            var point = Math.ceil(Math.random() * 20);
+            this.player_heal += point;
+            this.monster_attack();
+        },
+        give_up : function(){
+            this.player_heal = 0;
+        },
+        monster_attack : function(){
+            var point = Math.ceil(Math.random() * 15);
+            this.player_heal -= point;
+        }
+    },
+    watch : {
+        player_heal : function(value){
+            if(value < 0){
+                this.player_heal = 0;
+                if(confirm("Oyunu KAYBETTİN. Tekrar denemek ister misin?")){
+                    this.player_heal = 100;
+                    this.monster_heal = 100;
+                }
+            }else if(value > 100){
+                this.player_heal = 100;
+            }
+        },
+        monster_heal : function(value){
+            if(value < 0){
+                this.monster_heal = 0;
+                if(confirm("Oyunu KAZANDIN. Tekrar oynamak ister misin?")){
+                    this.player_heal = 100;
+                    this.monster_heal = 100;
+                }
+            }else if(value > 100){
+                this.monster_heal = 100;
+            }
+        }
+    }
+})
